aa88fbfe95caa118ad3807d66a6c93e3c5eddc68
Angel Mora <105449326+angelmora2004@users.noreply.github.com>
2024-12-27 17:28:59 -0500

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-black text-gray-400 py-4 text-center">
-      <p>&copy; 2024 MotorHub. Todos los derechos reservados.</p>
-    </footer>
-  );
-};
-
-export default Footer; 
